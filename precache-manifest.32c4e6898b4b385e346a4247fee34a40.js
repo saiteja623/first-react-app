9110d5d97e3e9c56b996712fcfc90bb629c13247
saiteja623 <saiteja.balla13@gmail.com>
2020-06-18 10:37:39 +0530

    Updates

---
+++ precache-manifest.32c4e6898b4b385e346a4247fee34a40.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177a3002404f925a761af3c3fdd01836",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "b2aa057fd4d13d667808",
+    "url": "/first-react-app/static/css/main.3504a65a.chunk.css"
+  },
+  {
+    "revision": "7a8a6649bd44c59b32f9",
+    "url": "/first-react-app/static/js/2.35940247.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first-react-app/static/js/2.35940247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2aa057fd4d13d667808",
+    "url": "/first-react-app/static/js/main.9ee5425f.chunk.js"
+  },
+  {
+    "revision": "b48c15141ac579a6bf34",
+    "url": "/first-react-app/static/js/runtime-main.58495b58.js"
+  },
+  {
+    "revision": "01dd8a518a015c9d534b3513f824be03",
+    "url": "/first-react-app/static/media/backgroun2.01dd8a51.jpg"
+  },
+  {
+    "revision": "7539d097167c508a5054c48c72202ded",
+    "url": "/first-react-app/static/media/background3.7539d097.jpeg"
+  },
+  {
+    "revision": "4551f79ceeb10d290cb6fcfdca09868f",
+    "url": "/first-react-app/static/media/calculator.4551f79c.jpg"
+  },
+  {
+    "revision": "ac02a7a559d73952107c31276e8b33ee",
+    "url": "/first-react-app/static/media/computer.ac02a7a5.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/first-react-app/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/first-react-app/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/first-react-app/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/first-react-app/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/first-react-app/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/first-react-app/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/first-react-app/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/first-react-app/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/first-react-app/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/first-react-app/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/first-react-app/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/first-react-app/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/first-react-app/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/first-react-app/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/first-react-app/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "4c6e19b6471fc90c608c6b13d41453c0",
+    "url": "/first-react-app/static/media/lightning.4c6e19b6.jpg"
+  },
+  {
+    "revision": "6e6720a128732b20871787b3da5f089f",
+    "url": "/first-react-app/static/media/quiz.6e6720a1.png"
+  },
+  {
+    "revision": "b7ab662a56aae3466e4e3c409f7099d9",
+    "url": "/first-react-app/static/media/socialmedia.b7ab662a.png"
+  },
+  {
+    "revision": "569e9c2bbb2675cda0a9295939fc9e52",
+    "url": "/first-react-app/static/media/todo.569e9c2b.jpg"
+  },
+  {
+    "revision": "1d703d498b17af05094cb6e6e6043239",
+    "url": "/first-react-app/static/media/typing1.1d703d49.jpg"
+  },
+  {
+    "revision": "86810fbe6d144dd5425010d64d5899ec",
+    "url": "/first-react-app/static/media/weather.86810fbe.png"
+  },
+  {
+    "revision": "d4fbbf0391342738aac5bd1dfada91fc",
+    "url": "/first-react-app/static/media/weather5.d4fbbf03.jpg"
+  },
+  {
+    "revision": "9dbf363385a2643a8c1960d15f27f3d2",
+    "url": "/first-react-app/static/media/weather6.9dbf3633.jpg"
+  },
+  {
+    "revision": "36495d091281190c4328262a40cfbfd2",
+    "url": "/first-react-app/static/media/weather7.36495d09.jpg"
+  },
+  {
+    "revision": "6e00940691929b3a08decd587d8bc344",
+    "url": "/first-react-app/static/media/weather8.6e009406.jpg"
+  },
+  {
+    "revision": "42e491afdbaf672f1bc0175091438a90",
+    "url": "/first-react-app/static/media/winner.42e491af.png"
+  }
+]);
