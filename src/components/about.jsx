c0e407bbcf11fa2bf143ad4a365c0068e7656222
saiteja623 <saiteja.balla13@gmail.com>
2020-06-18 10:32:51 +0530

    first-commit

---
+++ src/components/about.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import "./css/about.css";
+import "./fontawesome/css/all.css";
+
+class About extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="about-container">
+          <img src={require("./computer.jpg")}></img>
+          <p>
+            Hello There,This is basically a website of games and
+            applications.This is fully front-end website built using
+            React-Js.Thanks for visiting the website.
+          </p>
+          <div className="social-media">
+            <img src={require("./socialmedia.png")}></img>
+          </div>
+          <div className="icons">
+            <p>Any suggestions would be appreciated</p>
+            <a href="">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="https://github.com/saiteja623/" target="_blank">
+              <i className="fab fa-github"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/saiteja_balla0713/"
+              target="_blank"
+            >
+              <i class="fab fa-instagram" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default About;
